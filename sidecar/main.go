606f56f45bbbb41723024ce2b501dbe946d90175
Alex Collins <alex_collins@intuit.com>
2021-03-26 00:55:48 -0700

    improved

--- sidecar/main.go
+++ sidecar/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,7 +20,13 @@ import (
 	dfv1 "github.com/argoproj-labs/argo-dataflow/api/v1alpha1"
 )
 
-var log = klogr.New()
+var (
+	log            = klogr.New()
+	pipelineName   = os.Getenv("PIPELINE_NAME")
+	deploymentName = os.Getenv("DEPLOYMENT_NAME")
+	node           = &dfv1.Node{}
+	noopCloser     = func() error { return nil }
+)
 
 const varRun = "/var/run/argo-dataflow"
 
@@ -51,12 +58,10 @@ func (h handler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.Con
 func mainE() error {
 	ctx := signals.SetupSignalHandler()
 
-	deploymentName := os.Getenv("DEPLOYMENT_NAME")
-	node := &dfv1.Node{}
 	if err := json.Unmarshal([]byte(os.Getenv("NODE")), node); err != nil {
 		return err
 	}
-	log.WithValues("node", node, "deploymentName", deploymentName).Info("config")
+	log.WithValues("node", node, "deploymentName", deploymentName, "pipelineName", pipelineName).Info("config")
 
 	config := sarama.NewConfig()
 	config.ClientID = "dataflow-sidecar"
@@ -70,42 +75,119 @@ func mainE() error {
 	}
 	defer nc.Close()
 
-	var mainToSink func([]byte) error
-	for _, sink := range node.Sinks {
-		if sink.Bus != nil {
-			mainToSink = func(data []byte) error {
-				log.Info("sending message from main to kafka")
-				return nc.Publish(sink.Bus.Subject, data)
-			}
-		} else if sink.Kafka != nil {
-			producer, err := sarama.NewAsyncProducer([]string{sink.Kafka.URL}, config)
-			if err != nil {
-				return fmt.Errorf("failed to create kafka producer: %w", err)
-			}
-			mainToSink = func(data []byte) error {
-				log.Info("sending message from main to kafka")
-				producer.Input() <- &sarama.ProducerMessage{
-					Topic: sink.Kafka.Topic,
-					Value: sarama.StringEncoder(data),
-				}
-				return nil
-			}
-		} else {
-			return fmt.Errorf("sink misconfigured")
-		}
+	toSink, err := connnectSink(nc, config)
+	if err != nil {
+		return err
 	}
 
-	if node.Out == nil {
-		mainToSink = func(i []byte) error {
-			return fmt.Errorf("no out interface configured")
+	if closer, err := connectOut(toSink); err != nil {
+		return err
+	} else {
+		defer closer()
+	}
+
+	closer, toMain, err := connectTo()
+	if err != nil {
+		return err
+	}
+	defer closer()
+
+	if err := connectSources(ctx, nc, config, toMain); err != nil {
+		return err
+	}
+
+	log.Info("ready")
+
+	<-ctx.Done()
+
+	return nil
+}
+
+func connectSources(ctx context.Context, nc *nats.Conn, config *sarama.Config, toMain func([]byte) error) error {
+	for _, source := range node.Sources {
+		if source.Bus != nil {
+			subject := "pipeline." + pipelineName + "." + source.Bus.Subject
+			log.Info("connecting source", "type", "bus", "subject", subject)
+			if _, err := nc.QueueSubscribe(subject, deploymentName, func(m *nats.Msg) {
+				if err := toMain(m.Data); err != nil {
+					log.Error(err, "failed to send message from main to bus")
+				} else {
+					log.Info("sent message from main to bus", "subject", subject)
+				}
+			}); err != nil {
+				return fmt.Errorf("failed to subscribe: %w", err)
+			}
+		} else if source.Kafka != nil {
+			log.Info("connecting source", "type", "kafka", "subject", source.Kafka.Topic)
+			group, err := sarama.NewConsumerGroup([]string{source.Kafka.URL}, deploymentName, config)
+			if err != nil {
+				return fmt.Errorf("failed to create kafka consumer group: %w", err)
+			}
+			if err := group.Consume(ctx, []string{source.Kafka.Topic}, handler{toMain}); err != nil {
+				return fmt.Errorf("failed to create kafka consumer: %w", err)
+			}
+		} else {
+			return fmt.Errorf("source misconfigured")
 		}
+	}
+	return nil
+}
+
+func connectTo() (func() error, func([]byte) error, error) {
+	if node.In == nil {
+		log.Info("no in interface configured")
+		return noopCloser, func(i []byte) error {
+			return fmt.Errorf("no in interface configured")
+		}, nil
+	} else if node.In.FIFO {
+		log.Info("FIFO in interface configured")
+		path := filepath.Join(varRun, "in")
+		fifo, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, os.ModeNamedPipe)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to open input FIFO: %w", err)
+		}
+		log.WithValues("path", path).Info("opened input FIFO")
+		return fifo.Close, func(data []byte) error {
+			log.Info("sending message from source to main via FIFO")
+			if _, err := fifo.Write(data); err != nil {
+				return fmt.Errorf("failed to write message from source to main via FIFO: %w", err)
+			}
+			if _, err := fifo.Write([]byte("\n")); err != nil {
+				return fmt.Errorf("failed to write EOL from source to main via FIFO: %w", err)
+			}
+			log.Info("sent message from source to main via FIFO")
+			return nil
+		}, nil
+	} else if node.In.HTTP != nil {
+		log.Info("HTTP in interface configured")
+		return noopCloser, func(data []byte) error {
+			log.Info("sending message from source to main via HTTP")
+			resp, err := http.Post("http://localhost:8080/messages", "application/json", bytes.NewBuffer(data))
+			if err != nil {
+				return fmt.Errorf("failed to sent message from source to main via HTTP: %w", err)
+			}
+			if resp.StatusCode >= 300 {
+				return fmt.Errorf("failed to sent message from source to main via HTTP: %s", resp.Status)
+			}
+			log.Info("sent message from source to main via HTTP")
+			return nil
+		}, nil
+	} else {
+		return nil, nil, fmt.Errorf("in interface misconfigured")
+	}
+}
+
+func connectOut(toSink func([]byte) error) (func() error, error) {
+	if node.Out == nil {
+		log.Info("no out interface configured")
+		return noopCloser, nil
 	} else if node.Out.FIFO {
+		log.Info("FIFO out interface configured")
 		path := filepath.Join(varRun, "out")
 		fifo, err := os.OpenFile(path, os.O_RDONLY, os.ModeNamedPipe)
 		if err != nil {
-			return fmt.Errorf("failed to open output FIFO: %w", err)
+			return nil, fmt.Errorf("failed to open output FIFO: %w", err)
 		}
-		defer func() { _ = fifo.Close() }()
 		log.WithValues("path", path).Info("opened output FIFO")
 		go func() {
 			runtime.HandleCrash(runtime.PanicHandlers...)
@@ -113,7 +195,7 @@ func mainE() error {
 				scanner := bufio.NewScanner(fifo)
 				for scanner.Scan() {
 					log.Info("received message from main via FIFO")
-					if err := mainToSink(scanner.Bytes()); err != nil {
+					if err := toSink(scanner.Bytes()); err != nil {
 						return fmt.Errorf("failed to send message from main to sink: %w", err)
 					}
 				}
@@ -127,7 +209,9 @@ func mainE() error {
 				os.Exit(1)
 			}
 		}()
+		return fifo.Close, nil
 	} else if node.Out.HTTP != nil {
+		log.Info("HTTP out interface configured")
 		http.HandleFunc("/messages", func(w http.ResponseWriter, r *http.Request) {
 			data, err := ioutil.ReadAll(r.Body)
 			if err != nil {
@@ -136,7 +220,7 @@ func mainE() error {
 				return
 			}
 			log.Info("received message from main via HTTP")
-			if err := mainToSink(data); err != nil {
+			if err := toSink(data); err != nil {
 				log.Error(err, "failed to send message from main to sink")
 				w.WriteHeader(500)
 				return
@@ -153,80 +237,39 @@ func mainE() error {
 				os.Exit(1)
 			}
 		}()
+		return func() error { return nil }, nil
 	} else {
-		return fmt.Errorf("out interface misconfigured")
+		return nil, fmt.Errorf("out interface misconfigured")
 	}
+}
 
-	var sourceToMain func([]byte) error
-
-	if node.In == nil {
-		sourceToMain = func(i []byte) error {
-			return fmt.Errorf("no in interface configured")
-		}
-	} else if node.In.FIFO {
-		path := filepath.Join(varRun, "in")
-		fifo, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, os.ModeNamedPipe)
-		if err != nil {
-			return fmt.Errorf("failed to open input FIFO: %w", err)
-		}
-		defer func() { _ = fifo.Close() }()
-		log.WithValues("path", path).Info("opened input FIFO")
-		sourceToMain = func(data []byte) error {
-			log.Info("sending message from source to main via FIFO")
-			if _, err := fifo.Write(data); err != nil {
-				return fmt.Errorf("failed to write message from source to main via FIFO: %w", err)
+func connnectSink(nc *nats.Conn, config *sarama.Config) (func([]byte) error, error) {
+	var toSink func([]byte) error
+	for _, sink := range node.Sinks {
+		if sink.Bus != nil {
+			subject := "pipeline." + pipelineName + "." + sink.Bus.Subject
+			log.Info("connecting sink", "type", "bus", "subject", subject)
+			toSink = func(data []byte) error {
+				log.Info("sending message from main to bus", "subject", subject)
+				return nc.Publish(subject, data)
 			}
-			if _, err := fifo.Write([]byte("\n")); err != nil {
-				return fmt.Errorf("failed to write EOL from source to main via FIFO: %w", err)
-			}
-			log.Info("sent message from source to main via FIFO")
-			return nil
-		}
-	} else if node.In.HTTP != nil {
-		sourceToMain = func(data []byte) error {
-			log.Info("sending message from source to main via HTTP")
-			resp, err := http.Post("http://localhost:8080/messages", "application/json", bytes.NewBuffer(data))
+		} else if sink.Kafka != nil {
+			log.Info("connecting sink", "type", "kafka", "subject", sink.Kafka.Topic)
+			producer, err := sarama.NewAsyncProducer([]string{sink.Kafka.URL}, config)
 			if err != nil {
-				return fmt.Errorf("failed to sent message from source to main via HTTP: %w", err)
+				return nil, fmt.Errorf("failed to create kafka producer: %w", err)
 			}
-			if resp.StatusCode >= 300 {
-				return fmt.Errorf("failed to sent message from source to main via HTTP: %s", resp.Status)
-			}
-			log.Info("sent message from source to main via HTTP")
-			return nil
-		}
-	} else {
-		return fmt.Errorf("in interface misconfigured")
-	}
-
-	for _, source := range node.Sources {
-		if source.Bus != nil {
-			if _, err := nc.QueueSubscribe(source.Bus.Subject, deploymentName, func(m *nats.Msg) {
-				if err := sourceToMain(m.Data); err != nil {
-					log.Error(err, "failed to send message from main to bus")
-				} else {
-					log.Info("sent message from main to bus")
+			toSink = func(data []byte) error {
+				log.Info("sending message from main to kafka", "topic", sink.Kafka.Topic)
+				producer.Input() <- &sarama.ProducerMessage{
+					Topic: sink.Kafka.Topic,
+					Value: sarama.StringEncoder(data),
 				}
-			}); err != nil {
-				return fmt.Errorf("failed to subscribe: %w", err)
-			}
-		} else if source.Kafka != nil {
-			group, err := sarama.NewConsumerGroup([]string{source.Kafka.URL}, deploymentName, config)
-			if err != nil {
-				return fmt.Errorf("failed to create kafka consumer group: %w", err)
-			}
-			defer func() { _ = group.Close() }()
-			if err := group.Consume(ctx, []string{source.Kafka.Topic}, handler{sourceToMain}); err != nil {
-				return fmt.Errorf("failed to create kafka consumer: %w", err)
+				return nil
 			}
 		} else {
-			return fmt.Errorf("source misconfigured")
+			return nil, fmt.Errorf("sink misconfigured")
 		}
 	}
-
-	log.Info("ready")
-
-	<-ctx.Done()
-
-	return nil
+	return toSink, nil
 }
