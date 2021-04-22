814209838cd7b285a72048b28319dc415122d5b4
Alex Collins <alex_collins@intuit.com>
2021-04-22 15:14:21 -0700

    chore: removed init func 2/2

--- controllers/config.go
+++ controllers/config.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-	dfv1 "github.com/argoproj-labs/argo-dataflow/api/v1alpha1"
 	"os"
 	"time"
 
@@ -18,23 +16,3 @@ var (
 	updateInterval = 15 * time.Second
 	installer      = os.Getenv("INSTALLER") == "true"
 )
-
-func init() {
-	if imageFormat == "" {
-		imageFormat = "quay.io/argoproj/%s:latest"
-	}
-	runnerImage = fmt.Sprintf(imageFormat, "dataflow-runner")
-	if text, ok := os.LookupEnv(dfv1.EnvUpdateInterval); ok {
-		if v, err := time.ParseDuration(text); err != nil {
-			panic(fmt.Errorf("failed to parse duration %q: %w", text, err))
-		} else {
-			updateInterval = v
-		}
-	}
-	logger.Info("config",
-		"imageFormat", imageFormat,
-		"runnerImage", runnerImage,
-		"pullPolicy", pullPolicy,
-		"installer", installer,
-		"updateInterval", updateInterval.String())
-}
