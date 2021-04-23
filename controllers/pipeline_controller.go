e41400760ae7f6b12023a10a50c6e8f97c70e57c
Alex Collins <alex_collins@intuit.com>
2021-04-23 16:38:23 -0700

    ok

--- controllers/pipeline_controller.go
+++ controllers/pipeline_controller.go
@@ -20,16 +20,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-logr/logr"
-	apierr "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/argoproj-labs/argo-dataflow/api/util"
 	"github.com/argoproj-labs/argo-dataflow/api/util/containerkiller"
 	dfv1 "github.com/argoproj-labs/argo-dataflow/api/v1alpha1"
 	"github.com/argoproj-labs/argo-dataflow/controllers/bus"
@@ -53,7 +49,7 @@ type PipelineReconciler struct {
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=create;get;delete
 // +kubebuilder:rbac:groups=,resources=secrets,verbs=create;get;delete
 func (r *PipelineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := r.Log.WithValues("pipeline", req.NamespacedName)
+	_ = r.Log.WithValues("pipeline", req.NamespacedName)
 
 	pipeline := &dfv1.Pipeline{}
 	if err := r.Get(ctx, req.NamespacedName, pipeline); err != nil {
@@ -82,48 +78,6 @@ func (r *PipelineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 				}
 			}
 		}
-	}
-
-	log.Info("reconciling", "steps", len(pipeline.Spec.Steps))
-
-	for _, step := range pipeline.Spec.Steps {
-		stepFullName := pipeline.Name + "-" + step.Name
-		log.Info("applying step", "stepName", step.Name, "stepFullName", stepFullName)
-		matchLabels := map[string]string{dfv1.KeyPipelineName: pipeline.Name, dfv1.KeyStepName: step.Name}
-		obj := &dfv1.Step{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: pipeline.Namespace,
-				Name:      stepFullName,
-				Labels:    matchLabels,
-				OwnerReferences: []metav1.OwnerReference{
-					*metav1.NewControllerRef(pipeline.GetObjectMeta(), dfv1.PipelineGroupVersionKind),
-				},
-			},
-			Spec: step,
-		}
-		if err := r.Client.Create(ctx, obj); err != nil {
-			if apierr.IsAlreadyExists(err) {
-				old := &dfv1.Step{}
-				if err := r.Client.Get(ctx, client.ObjectKeyFromObject(obj), old); err != nil {
-					return ctrl.Result{}, err
-				}
-				if util.NotEqual(step, old.Spec) {
-					log.Info("updating step due to changed spec")
-					old.Spec = step
-					if err := r.Client.Update(ctx, old); dfv1.IgnoreConflict(err) != nil { // ignore conflicts, we will be reconciling again shortly if this happens
-						return ctrl.Result{}, err
-					}
-				}
-			} else {
-				return ctrl.Result{}, fmt.Errorf("failed to created step %s: %w", obj.GetName(), err)
-			}
-		}
-	}
-
-	steps := &dfv1.StepList{}
-	selector, _ := labels.Parse(dfv1.KeyPipelineName + "=" + pipeline.Name)
-	if err := r.Client.List(ctx, steps, &client.ListOptions{Namespace: pipeline.Namespace, LabelSelector: selector}); err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to list steps: %w", err)
 	}
 
 	return ctrl.Result{}, nil
