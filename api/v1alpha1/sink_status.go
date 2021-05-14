1fbfbd1fe690ddf508ca0b6aa8bedda3ed7ad7ba
Alex Collins <alex_collins@intuit.com>
2021-05-13 17:21:35 -0700

    fix: correct `error` to `lastError`

--- api/v1alpha1/sink_status.go
+++ api/v1alpha1/sink_status.go
@@ -2,7 +2,7 @@ package v1alpha1
 
 type SinkStatus struct {
 	LastMessage *Message           `json:"lastMessage,omitempty" protobuf:"bytes,2,opt,name=lastMessage"`
-	LastError   *Error             `json:"error,omitempty" protobuf:"bytes,4,opt,name=error"`
+	LastError   *Error             `json:"lastError,omitempty" protobuf:"bytes,4,opt,name=lastError"`
 	Metrics     map[string]Metrics `json:"metrics,omitempty" protobuf:"bytes,3,rep,name=metrics"`
 }
 
--- api/v1alpha1/source_status.go
+++ api/v1alpha1/source_status.go
@@ -2,7 +2,7 @@ package v1alpha1
 
 type SourceStatus struct {
 	LastMessage *Message           `json:"lastMessage,omitempty" protobuf:"bytes,2,opt,name=lastMessage"`
-	LastError   *Error             `json:"error,omitempty" protobuf:"bytes,5,opt,name=error"`
+	LastError   *Error             `json:"lastError,omitempty" protobuf:"bytes,5,opt,name=lastError"`
 	Pending     *uint64            `json:"pending,omitempty" protobuf:"varint,3,opt,name=pending"`
 	Metrics     map[string]Metrics `json:"metrics,omitempty" protobuf:"bytes,4,rep,name=metrics"`
 }
