dd70bab1a37c46b2e51747404ad31f3a15e4e1ca
Alex Collins <alex_collins@intuit.com>
2021-06-05 13:58:51 -0700

    test: add test

---
+++ shared/util/print_test.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIsPrint(t *testing.T) {
+	assert.True(t, IsPrint(""))
+	assert.True(t, IsPrint("abc"))
+	assert.False(t, IsPrint("\000"))
+}
+
+func TestPrintable(t *testing.T) {
+	assert.Equal(t, "", Printable(""))
+	assert.Equal(t, "abc", Printable("abc"))
+	assert.Equal(t, "AA==", Printable("\000"))
+}
+
