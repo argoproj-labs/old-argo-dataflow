fd16936373e99b19e5d34cd0dfaa4bcb7c0f1f4b
Alex Collins <alex_collins@intuit.com>
2021-06-06 18:09:17 -0700

    chore: remove formatting

--- examples/main.go
+++ examples/main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
-
 	"github.com/argoproj-labs/argo-dataflow/shared/util"
-	"sigs.k8s.io/yaml"
 )
 
 func main() {
@@ -19,25 +14,15 @@ func main() {
 	_, _ = fmt.Printf("### Examples\n\n")
 	for _, info := range files {
 		println(info.Name())
-		formatted := make([]string, len(info.Items))
 		for i, un := range info.Items {
 			if i == 0 {
 				annotations := un.GetAnnotations()
-				if name, ok := annotations["dataflow.argoproj.io/name"]; ok {
-					title := name
-					_, _ = fmt.Printf("### [%s](https://raw.githubusercontent.com/argoproj-labs/argo-dataflow/main/examples/%s)\n\n", title, info.Name())
+				if _, ok := annotations["dataflow.argoproj.io/description"]; ok {
+					_, _ = fmt.Printf("### [%s](https://raw.githubusercontent.com/argoproj-labs/argo-dataflow/main/examples/%s)\n\n", un.GetName(), info.Name())
 					_, _ = fmt.Printf("%s\n\n", annotations["dataflow.argoproj.io/description"])
 					_, _ = fmt.Printf("```\nkubectl apply -f https://raw.githubusercontent.com/argoproj-labs/argo-dataflow/main/examples/%s\n```\n\n", info.Name())
 				}
 			}
-			if data, err := yaml.Marshal(un.Object); err != nil {
-				panic(err)
-			} else {
-				formatted[i] = string(data)
-			}
-		}
-		if err := ioutil.WriteFile(filepath.Join(dirname, info.Name()), []byte(strings.Join(formatted, "\n---\n")), 0o600); err != nil {
-			panic(err)
 		}
 	}
 }
