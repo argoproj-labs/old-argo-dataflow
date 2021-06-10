a411be82d23310376b416b23ad9a353473ee300a
Alex Collins <alex_collins@intuit.com>
2021-06-10 12:12:37 -0700

    feat: publish Python library

--- dsls/python/setup.py
+++ dsls/python/setup.py
@@ -1,25 +1,21 @@
 from distutils.core import setup
 
 setup(
-    name='argo-dataflow',  # How you named your package folder (MyLib)
-    packages=['.'],  # Chose the same as "name"
-    version='v0.0.48',  # Start with a small number and increase it with every change you make
-    license='apache-2.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description='Argo Dataflow',  # Give a short description about your library
-    author='Alex Collins',  # Type in your name
-    author_email='alex_collins@intuit.com',  # Type in your E-Mail
-    url='https://github.com/argoproj-labs/argo-dataflow',  # Provide either the link to your github or to your website
-    download_url='https://github.com/argoproj-labs/argo-dataflow/archive/v_01.tar.gz',  # I explain this later on
-    keywords=['Argo', 'Kubernetes'],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
-    ],
+    name='argo_dataflow',
+    packages=['argo_dataflow'],
+    version='v0.0.48',
+    license='apache-2.0',
+    description='Argo Dataflow',
+    author='Alex Collins',
+    author_email='alex_collins@intuit.com',
+    url='https://github.com/argoproj-labs/argo-dataflow',
+    keywords=['Argo', 'Kubernetes'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache Software License',  # Again, pick a license
-        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
