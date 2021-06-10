75997f661f1852a2ae7fd9e04319146ad6f4efd5
Alex Collins <alex_collins@intuit.com>
2021-06-09 17:38:14 -0700

    docs: update 301-kafka-pipeline.py

--- examples/301-kafka-pipeline.py
+++ examples/301-kafka-pipeline.py
@@ -3,7 +3,10 @@ from dsls.python import pipeline, kafka
 if __name__ == '__main__':
     (pipeline("301-kafka")
      .owner('argoproj-labs')
-     .describe("""This example shows reading and writing to a Kafka topic""")
+     .describe("""This example shows reading and writing to a Kafka topic
+     
+Kafka topics are typically partitioned. Dataflow will process each partition simultaneously.     
+     """)
      .annotate("dataflow.argoproj.io/test", "true")
      .step(
         (kafka('input-topic')
