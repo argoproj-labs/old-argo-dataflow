2db856393fff4c51a185c394a3063298de3750b6
Alex Collins <alex_collins@intuit.com>
2021-06-24 17:22:10 -0700

    test: enable GCP

--- test/fixtures.go
+++ test/fixtures.go
@@ -5,6 +5,7 @@ package test
 import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"testing"
 )
