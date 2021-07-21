0d3a58c3cb1ec90eb2cc7daf84dfa7e6d3d93ee1
Alex Collins <alex_collins@intuit.com>
2021-07-21 16:05:45 -0700

    chore: lint

--- runner/sidecar/print.go
+++
@@ -1,8 +0,0 @@
-package sidecar
-
-import sharedutil "github.com/argoproj-labs/argo-dataflow/shared/util"
-
-// format or redact message
-func printable(m []byte) string {
-	return sharedutil.Printable(string(m))
-}
