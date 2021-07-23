20f259a96f38994a394bc556a8b79c4eb81efa1c
Alex Collins <alex_collins@intuit.com>
2021-07-23 14:30:35 -0700

    test: removed unused code

--- test/stan.go
+++ test/stan.go
@@ -29,13 +29,3 @@ func PumpSTANSubject(subject string, n int, opts ...interface{}) {
 	log.Printf("puming stan subject %q sleeping %v with %d messages\n", subject, sleep, n)
 	InvokeTestAPI("/stan/pump-subject?subject=%s&sleep=%v&n=%d&prefix=%s", subject, sleep, n, prefix)
 }
-
-func RestartSTAN() {
-	DeletePod("nats-0")
-	DeletePod("stan-0")
-}
-
-func WaitForSTAN() {
-	WaitForPod("nats-0")
-	WaitForPod("stan-0")
-}
