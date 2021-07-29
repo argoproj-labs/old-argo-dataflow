16c1b30ef1a2ab791a93bd2307e749cba6c62b12
Dominik Deren <dominik.deren@live.com>
2021-07-29 16:43:59 +0200

    Updating git step example to present how to use python runtime with the new python sdk.

--- examples/106-git-pipeline.py
+++ examples/106-git-pipeline.py
@@ -6,13 +6,18 @@ if __name__ == '__main__':
      .describe("""This example of a pipeline using Git.
 
 The Git handler allows you to check your application source code into Git. Dataflow will checkout and build
-your code when the step starts.
+your code when the step starts. This example presents how one can use both go runtime & python runtime git steps.
 
 [Learn about Git steps](../docs/GIT.md)""")
      .step(
         (kafka('input-topic')
          .git('main', 'https://github.com/argoproj-labs/argo-dataflow', 'main', 'examples/git', 'golang:1.16',
               env=[{'name': "GOCACHE", 'value': "/tmp/.gocache"}], command=["go", "run", "."])
-         .kafka('output-topic')
+         .kafka('go-output-topic')
+         ))
+     .step(
+        (kafka('input-topic')
+         .git('main', 'https://github.com/argoproj-labs/argo-dataflow', 'main', 'examples/git-python', 'python:3.9', command=["./start.sh"])
+         .kafka('python-output-topic')
          ))
      .save())
