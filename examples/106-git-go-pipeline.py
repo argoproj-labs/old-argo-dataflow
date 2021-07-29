2a06148ecafc34cd995e9bcb9eb02f7632ba21fd
Dominik Deren <dominik.deren@live.com>
2021-07-29 17:59:29 +0200

    Splitting an example into two.

--- examples/106-git-go-pipeline.py
+++ examples/106-git-go-pipeline.py
@@ -1,23 +1,18 @@
 from argo_dataflow import pipeline, kafka
 
 if __name__ == '__main__':
-    (pipeline("106-git")
+    (pipeline("106-git-go")
      .owner('argoproj-labs')
      .describe("""This example of a pipeline using Git.
 
 The Git handler allows you to check your application source code into Git. Dataflow will checkout and build
-your code when the step starts. This example presents how one can use both go runtime & python runtime git steps.
+your code when the step starts. This example presents how one can use go runtime git step.
 
 [Learn about Git steps](../docs/GIT.md)""")
      .step(
         (kafka('input-topic')
          .git('main', 'https://github.com/argoproj-labs/argo-dataflow', 'main', 'examples/git', 'golang:1.16',
               env=[{'name': "GOCACHE", 'value': "/tmp/.gocache"}], command=["go", "run", "."])
-         .kafka('go-output-topic')
-         ))
-     .step(
-        (kafka('input-topic')
-         .git('main', 'https://github.com/argoproj-labs/argo-dataflow', 'main', 'examples/git-python', 'python:3.9', command=["./start.sh"])
-         .kafka('python-output-topic')
+         .kafka('output-topic')
          ))
      .save())
---
+++ examples/106-git-python-pipeline.py
@@ -0,0 +1,17 @@
+from argo_dataflow import pipeline, kafka
+
+if __name__ == '__main__':
+    (pipeline("106-git-python")
+     .owner('argoproj-labs')
+     .describe("""This example of a pipeline using Git.
+
+The Git handler allows you to check your application source code into Git. Dataflow will checkout and build
+your code when the step starts. This example presents how one can use python runtime git step.
+
+[Learn about Git steps](../docs/GIT.md)""")
+     .step(
+        (kafka('input-topic')
+         .git('main', 'https://github.com/argoproj-labs/argo-dataflow', 'main', 'examples/git-python', 'python:3.9', command=["./start.sh"])
+         .kafka('output-topic')
+         ))
+     .save())
