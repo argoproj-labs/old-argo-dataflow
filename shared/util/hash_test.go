efa15a63429caf6262b3ea8f9bb596d8532a0ff9
Alex Collins <alexec@users.noreply.github.com>
2021-08-05 11:19:23 -0700

    feat: change hash from b64 to hex (#203)

--- shared/util/hash_test.go
+++ shared/util/hash_test.go
@@ -7,5 +7,5 @@ import (
 )
 
 func TestMustHash(t *testing.T) {
-	assert.Equal(t, "LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=", MustHash([]byte("foo")))
+	assert.Equal(t, "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", MustHash([]byte("foo")))
 }
