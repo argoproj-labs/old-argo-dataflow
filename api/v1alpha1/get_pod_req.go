d3a7a7222261174c1e777d6507b184bae598b0e7
Alex Collins <alex_collins@intuit.com>
2021-08-09 14:50:42 -0700

    feat: Enable config of sidecar resources

--- api/v1alpha1/get_pod_req.go
+++ api/v1alpha1/get_pod_req.go
@@ -16,4 +16,5 @@ type GetPodSpecReq struct {
 	PullPolicy     corev1.PullPolicy `protobuf:"bytes,6,opt,name=pullPolicy,casttype=k8s.io/api/core/v1.PullPolicy"`
 	UpdateInterval time.Duration     `protobuf:"varint,7,opt,name=updateInterval,casttype=time.Duration"`
 	StepStatus     StepStatus        `protobuf:"bytes,8,opt,name=stepStatus"`
+	Sidecar        Sidecar           `protobuf:"bytes,10,opt,name=sidecar"`
 }
--- api/v1alpha1/step_spec.go
+++ api/v1alpha1/step_spec.go
@@ -40,6 +40,8 @@ type StepSpec struct {
 	NodeSelector       map[string]string   `json:"nodeSelector,omitempty" protobuf:"bytes,17,rep,name=nodeSelector"`
 	Affinity           *corev1.Affinity    `json:"affinity,omitempty" protobuf:"bytes,18,opt,name=affinity"`
 	Tolerations        []corev1.Toleration `json:"tolerations,omitempty" protobuf:"bytes,19,rep,name=tolerations"`
+	// +kubebuilder:default={resources: {limits: {"cpu": "500m", "memory": "256Mi"}, requests: {"cpu": "100m", "memory": "64Mi"}}}
+	Sidecar Sidecar `json:"sidecar,omitempty" protobuf:"bytes,28,opt,name=sidecar"`
 }
 
 func (in StepSpec) GetIn() *Interface {
