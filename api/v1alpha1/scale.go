dab337c8caaebf535177759edc3277d4837592c8
Alex Collins <alexec@users.noreply.github.com>
2021-08-16 14:21:00 -0700

    feat!: Expression based scaling. Fixes #249 (#250)

--- api/v1alpha1/scale.go
+++ api/v1alpha1/scale.go
@@ -1,31 +1,12 @@
 package v1alpha1
 
 type Scale struct {
-	MinReplicas  int32   `json:"minReplicas" protobuf:"varint,1,opt,name=minReplicas"`
-	MaxReplicas  *uint32 `json:"maxReplicas,omitempty" protobuf:"varint,2,opt,name=maxReplicas"` // takes precedence over min
-	ReplicaRatio uint32  `json:"replicaRatio,omitempty" protobuf:"varint,3,opt,name=replicaRatio"`
-}
-
-// Used to calculate the number of replicas.
-// min(r.max, max(r.min, pending/ratio))
-// Example:
-// min=1, max=4, ratio=100
-// pending=0, replicas=1
-// pending=100, replicas=1
-// pending=200, replicas=2
-// pending=300, replicas=3
-// pending=400, replicas=4
-// pending=500, replicas=4
-func (in Scale) Calculate(pending int) int {
-	n := 0
-	if in.ReplicaRatio > 0 {
-		n = pending / int(in.ReplicaRatio)
-	}
-	if n < int(in.MinReplicas) {
-		n = int(in.MinReplicas)
-	}
-	if in.MaxReplicas != nil && n > int(*in.MaxReplicas) {
-		n = int(*in.MaxReplicas)
-	}
-	return n
+	// An expression to determine the number of replicas. Must evaluation to an `int`.
+	DesiredReplicas string `json:"desiredReplicas,omitempty" protobuf:"bytes,1,opt,name=desiredReplicas"`
+	// An expression to determine the delay for peeking. Maybe string or duration, e.g. `"4m"`
+	// +kubebuilder:default="defaultPeekDelay"
+	PeekDelay string `json:"peekDelay,omitempty" protobuf:"bytes,2,opt,name=peekDelay"`
+	// An expression to determine the delay for scaling. Maybe string or duration, e.g. `"1m"`
+	// +kubebuilder:default="defaultScalingDelay"
+	ScalingDelay string `json:"scalingDelay,omitempty" protobuf:"bytes,3,opt,name=scalingDelay"`
 }
---
+++ manager/controllers/scaling/scaling.go
@@ -0,0 +1,106 @@
+package scaling
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/antonmedv/expr"
+	dfv1 "github.com/argoproj-labs/argo-dataflow/api/v1alpha1"
+	sharedutil "github.com/argoproj-labs/argo-dataflow/shared/util"
+)
+
+var (
+	logger              = sharedutil.NewLogger()
+	defaultScalingDelay = sharedutil.GetEnvDuration(dfv1.EnvScalingDelay, time.Minute)
+	defaultPeekDelay    = sharedutil.GetEnvDuration(dfv1.EnvPeekDelay, 4*time.Minute)
+)
+
+func init() {
+	logger.Info("scaling config",
+		"defaultScalingDelay", defaultScalingDelay.String(),
+		"defaultPeekDelay", defaultPeekDelay.String(),
+	)
+}
+
+func GetDesiredReplicas(step dfv1.Step) (int, error) {
+	currentReplicas := int(step.Status.Replicas)
+	lastScaledAt := time.Since(step.Status.LastScaledAt.Time)
+	scale := step.Spec.Scale
+	var scalingDelay time.Duration
+	var peekDelay time.Duration
+	desiredReplicas := currentReplicas
+	{
+
+		var err error
+		if scalingDelay, err = evalAsDuration(scale.ScalingDelay, map[string]interface{}{
+			"defaultScalingDelay": defaultScalingDelay,
+		}); err != nil {
+			return 0, fmt.Errorf("failed to evaluate %q: %w", scale.ScalingDelay, err)
+		}
+		if peekDelay, err = evalAsDuration(scale.PeekDelay, map[string]interface{}{
+			"defaultPeekDelay": defaultPeekDelay,
+		}); err != nil {
+			return 0, fmt.Errorf("failed to evaluate %q: %w", scale.PeekDelay, err)
+		}
+		if scale.DesiredReplicas != "" {
+			pending := step.Status.SourceStatuses.GetPending()
+			c := int(step.Status.Replicas)
+			P := int(pending)
+			p := int(pending - step.Status.SourceStatuses.GetLastPending())
+			r, err := expr.Eval(scale.DesiredReplicas, map[string]interface{}{"c": c, "P": P, "p": p, "minmax": minmax})
+			if err != nil {
+				return 0, err
+			}
+			var ok bool
+			desiredReplicas, ok = r.(int)
+			if !ok {
+				return 0, fmt.Errorf("failed to evaluate %q as int, got %T", scale.DesiredReplicas, r)
+			}
+			logger.Info("desired replicas", "c", c, "P", P, "p", p, "d", desiredReplicas, "scalingDelay", scalingDelay, "peekDelay", peekDelay)
+		}
+	}
+	if lastScaledAt < scalingDelay {
+		return currentReplicas, nil
+	}
+	// do we need to peek? currentReplicas and desiredReplicas must both be zero
+	if currentReplicas <= 0 && desiredReplicas == 0 && lastScaledAt > peekDelay {
+		return 1, nil
+	}
+	// prevent violent scale-up and scale-down by only scaling by 1 each time
+	if desiredReplicas > currentReplicas {
+		return currentReplicas + 1, nil
+	} else if desiredReplicas < currentReplicas {
+		return currentReplicas - 1, nil
+	} else {
+		return desiredReplicas, nil
+	}
+}
+
+func evalAsDuration(input string, env map[string]interface{}) (time.Duration, error) {
+	if r, err := expr.Eval(input, env); err != nil {
+		return 0, err
+	} else {
+		switch v := r.(type) {
+		case string:
+			return time.ParseDuration(v)
+		case time.Duration:
+			return v, nil
+		default:
+			return 0, fmt.Errorf("wanted string, got to %T", r)
+		}
+	}
+}
+
+func RequeueAfter(step dfv1.Step, currentReplicas, desiredReplicas int) (time.Duration, error) {
+	if currentReplicas <= 0 && desiredReplicas == 0 {
+		scale := step.Spec.Scale
+		if scalingDelay, err := evalAsDuration(scale.ScalingDelay, map[string]interface{}{
+			"defaultScalingDelay": defaultScalingDelay,
+		}); err != nil {
+			return 0, fmt.Errorf("failed to evaluate %q: %w", scale.ScalingDelay, err)
+		} else {
+			return scalingDelay, nil
+		}
+	}
+	return 0, nil
+}
