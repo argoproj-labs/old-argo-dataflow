f0ada5fc0376ef7cb5c805a7e52c4adffe408a3a
Saravanan Balasubramanian <sarabala1979@gmail.com>
2021-08-16 15:32:44 -0700

    Update hash.go

--- shared/util/hash.go
+++ shared/util/hash.go
@@ -23,7 +23,7 @@ func MustHash(v interface{}) string {
 	}
 }
 
-func 	GetUniquePipelineID(clusterName, namespace, pipelineName, stepName, sourceName string) string {
+func GetUniquePipelineID(clusterName, namespace, pipelineName, stepName, sourceName string) string {
 	hash := MustHash(fmt.Sprintf("%s.%s.%s.%s.sources.%s", clusterName, namespace, pipelineName, stepName, sourceName))
 	return fmt.Sprintf("dataflow-%s-%s-%s-%s-%s-%s", strings.ShortenString(clusterName, 3), strings.ShortenString(namespace, 3), strings.ShortenString(pipelineName, 3), strings.ShortenString(stepName, 3), strings.ShortenString(sourceName, 3), hash)
 }
