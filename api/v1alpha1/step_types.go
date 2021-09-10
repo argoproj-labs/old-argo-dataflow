a1a0e63654f4498a3121c4e27dadbe827cf9b99a
Alex Collins <alexec@users.noreply.github.com>
2021-09-10 13:10:47 -0700

    chore: use field-path to simplify pod env var (#316)

--- api/v1alpha1/step_types.go
+++ api/v1alpha1/step_types.go
@@ -98,7 +98,7 @@ func (in Step) GetPodSpec(req GetPodSpecReq) corev1.PodSpec {
 	envVars := []corev1.EnvVar{
 		{Name: EnvCluster, Value: req.Cluster},
 		{Name: EnvDebug, Value: strconv.FormatBool(req.Debug)},
-		{Name: EnvNamespace, Value: req.Namespace},
+		{Name: EnvNamespace, ValueFrom: &corev1.EnvVarSource{FieldRef: &corev1.ObjectFieldSelector{FieldPath: "metadata.namespace"}}},
 		{Name: EnvPipelineName, Value: req.PipelineName},
 		{Name: EnvReplica, Value: strconv.Itoa(int(req.Replica))},
 		{Name: EnvStep, Value: string(step)},
