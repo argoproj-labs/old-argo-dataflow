bb4914e08db53b4b5ac58be8fd247c188f2aca9b
Alex Collins <alex_collins@intuit.com>
2021-09-13 10:53:32 -0700

    remove urner from sinks

--- api/v1alpha1/http_sink.go
+++ api/v1alpha1/http_sink.go
@@ -1,16 +1,7 @@
 package v1alpha1
 
-import (
-	"context"
-	"fmt"
-)
-
 type HTTPSink struct {
 	URL                string       `json:"url" protobuf:"bytes,1,opt,name=url"`
 	Headers            []HTTPHeader `json:"headers,omitempty" protobuf:"bytes,2,rep,name=headers"`
 	InsecureSkipVerify bool         `json:"insecureSkipVerify,omitempty" protobuf:"varint,3,opt,name=insecureSkipVerify"`
-}
-
-func (in HTTPSink) GetURN(context.Context) string {
-	return fmt.Sprintf("urn:dataflow:http:%s", in.URL)
 }
--- api/v1alpha1/sink.go
+++ api/v1alpha1/sink.go
@@ -1,10 +1,5 @@
 package v1alpha1
 
-import (
-	"context"
-	"fmt"
-)
-
 type Sink struct {
 	// +kubebuilder:default=default
 	Name   string      `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
@@ -15,27 +10,4 @@ type Sink struct {
 	S3     *S3Sink     `json:"s3,omitempty" protobuf:"bytes,6,opt,name=s3"`
 	DB     *DBSink     `json:"db,omitempty" protobuf:"bytes,7,opt,name=db"`
 	Volume *VolumeSink `json:"volume,omitempty" protobuf:"bytes,8,opt,name=volume"`
-}
-
-func (s Sink) get() urner {
-	if v := s.DB; v != nil {
-		return v
-	} else if v := s.HTTP; v != nil {
-		return v
-	} else if v := s.Kafka; v != nil {
-		return v
-	} else if v := s.Log; v != nil {
-		return v
-	} else if v := s.S3; v != nil {
-		return v
-	} else if v := s.STAN; v != nil {
-		return v
-	} else if v := s.Volume; v != nil {
-		return v
-	}
-	panic(fmt.Errorf("invalid sink %q", s.Name))
-}
-
-func (s Sink) GetURN(ctx context.Context) string {
-	return s.get().GetURN(ctx)
 }
