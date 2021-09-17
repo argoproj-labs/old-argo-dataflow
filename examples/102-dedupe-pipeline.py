6b299cc5aef4c7adbcc8b32af987f07d5ce25240
Alex Collins <alex_collins@intuit.com>
2021-09-17 15:18:39 -0700

    feat: Add `dedupe` to Python DSL
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

---
+++ examples/102-dedupe-pipeline.py
@@ -0,0 +1,12 @@
+from argo_dataflow import pipeline, kafka
+
+if __name__ == '__main__':
+    (pipeline("102-dedupe")
+     .owner('argoproj-labs')
+     .describe("""This is an example of built-in de-duplication step.""")
+     .step(
+        (kafka('input-topic')
+         .dedupe('name')
+         .kafka('output-topic'))
+    )
+     .save())
