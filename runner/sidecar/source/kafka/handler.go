306a90ef2c96e4d79636d0b19f79d0ef07a0f4cd
Alex Collins <alex_collins@intuit.com>
2021-09-17 15:58:24 -0700

    fix: run only one Kafka offset committer per step

--- runner/sidecar/source/kafka/handler.go
+++ runner/sidecar/source/kafka/handler.go
@@ -3,6 +3,7 @@ package kafka
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/Shopify/sarama"
@@ -14,39 +15,39 @@ import (
 )
 
 type handler struct {
-	sourceName string
-	sourceURN  string
-	process    source.Process
+	sourceName      string
+	sourceURN       string
+	process         source.Process
+	offsetCommitter bool
+	mu              sync.Mutex
 }
 
-func (h handler) Setup(sess sarama.ConsumerGroupSession) error {
+func newHandler(sourceName, sourceURN string, process source.Process) sarama.ConsumerGroupHandler {
+	return &handler{
+		sourceName:      sourceName,
+		sourceURN:       sourceURN,
+		process:         process,
+		offsetCommitter: false,
+		mu:              sync.Mutex{},
+	}
+}
+
+func (h *handler) Setup(sess sarama.ConsumerGroupSession) error {
 	logger.Info("Kafka handler set-up")
 	return nil
 }
 
-func (h handler) Cleanup(sess sarama.ConsumerGroupSession) error {
+func (h *handler) Cleanup(sess sarama.ConsumerGroupSession) error {
 	logger.Info("Kafka handler clean-up")
 	sess.Commit()
 	return nil
 }
 
-func (h handler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+func (h *handler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	defer runtime.HandleCrash()
 	ctx, cancel := context.WithCancel(sess.Context())
 	defer cancel()
-	go wait.JitterUntilWithContext(ctx, func(ctx context.Context) {
-		logger.Info("starting Kafka offset committer")
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ctx.Done():
-			case <-ticker.C:
-				sess.Commit()
-			}
-		}
-	}, time.Second, 1.2, true)
-	defer sess.Commit()
+	h.startOffsetCommitter(sess, ctx)
 	logger.Info("starting consuming claim", "partition", claim.Partition())
 	for msg := range claim.Messages() {
 		if err := h.processMessage(ctx, msg); err != nil {
@@ -58,7 +59,32 @@ func (h handler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.Con
 	return nil
 }
 
-func (h handler) processMessage(ctx context.Context, msg *sarama.ConsumerMessage) error {
+func (h *handler) startOffsetCommitter(sess sarama.ConsumerGroupSession, ctx context.Context) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	// this func will be called once per partition, but we only need one committer,
+	// so return if one exists
+	if h.offsetCommitter {
+		return
+	}
+	h.offsetCommitter = true
+	go wait.JitterUntilWithContext(ctx, func(ctx context.Context) {
+		logger.Info("starting Kafka offset committer")
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				h.offsetCommitter = false
+				sess.Commit()
+			case <-ticker.C:
+				sess.Commit()
+			}
+		}
+	}, time.Second, 1.2, true)
+}
+
+func (h *handler) processMessage(ctx context.Context, msg *sarama.ConsumerMessage) error {
 	defer runtime.HandleCrash()
 	span, ctx := opentracing.StartSpanFromContext(ctx, fmt.Sprintf("kafka-source-%s", h.sourceName))
 	defer span.Finish()
