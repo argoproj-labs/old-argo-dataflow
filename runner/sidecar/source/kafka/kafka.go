0cc494b4563eb393fa6290f36c9d6cc734551fb4
Alex Collins <alex_collins@intuit.com>
2021-09-30 14:52:04 -0700

    refactor to get clearer stack traces

--- runner/sidecar/source/kafka/kafka.go
+++ runner/sidecar/source/kafka/kafka.go
@@ -19,11 +19,14 @@ import (
 )
 
 type kafkaSource struct {
-	logger   logr.Logger
-	consumer *kafka.Consumer
-	topic    string
-	wg       *sync.WaitGroup
-	channels map[int32]chan *kafka.Message
+	logger     logr.Logger
+	sourceName string
+	sourceURN  string
+	consumer   *kafka.Consumer
+	topic      string
+	wg         *sync.WaitGroup
+	channels   map[int32]chan *kafka.Message
+	process    source.Process
 }
 
 func New(ctx context.Context, secretInterface corev1.SecretInterface, mntr monitor.Interface, consumerGroupID, sourceName, sourceURN string, replica int, x dfv1.KafkaSource, process source.Process) (source.Interface, error) {
@@ -48,113 +51,94 @@ func New(ctx context.Context, secretInterface corev1.SecretInterface, mntr monit
 		return nil, err
 	}
 
-	channels := map[int32]chan *kafka.Message{} // partition -> messages
-	processMessage := func(ctx context.Context, msg *kafka.Message) error {
-		span, ctx := opentracing.StartSpanFromContext(ctx, fmt.Sprintf("kafka-source-%s", sourceName))
-		defer span.Finish()
-		return process(
-			dfv1.ContextWithMeta(
-				ctx,
-				dfv1.Meta{
-					Source: sourceURN,
-					ID:     fmt.Sprintf("%d-%d", msg.TopicPartition.Partition, msg.TopicPartition.Offset),
-					Time:   msg.Timestamp.Unix(),
-				},
-			),
-			msg.Value,
-		)
-	}
-	// we need to make sure that before we close the consumer, we finish consuming the channels, otherwise we'll try
-	// to commit a message, but get an error
-	wg := &sync.WaitGroup{}
-	assignedPartition := func(partition int32) {
-		logger := logger.WithValues("partition", partition)
-		if _, ok := channels[partition]; !ok {
-			logger.Info("assigned partition")
-			channels[partition] = make(chan *kafka.Message, 256)
-			go wait.JitterUntilWithContext(ctx, func(ctx context.Context) {
-				logger.Info("consuming partition")
-				wg.Add(1)
-				defer func() {
-					logger.Info("done consuming partition")
-					wg.Done()
-				}()
-				for msg := range channels[partition] {
-					logger := logger.WithValues("offset", msg.TopicPartition.Offset)
-					if err := processMessage(ctx, msg); err != nil {
-						logger.Error(err, "failed to process message")
-					} else if _, err := consumer.CommitMessage(msg); err != nil {
-						logger.Error(err, "failed to commit message")
-					}
-				}
-			}, 3*time.Second, 1.2, true)
-		}
-	}
-
-	revokedPartition := func(partition int32) {
-		if _, ok := channels[partition]; ok {
-			logger.Info("revoked partition", "partition", partition)
-			close(channels[partition])
-			delete(channels, partition)
-		}
+	s := &kafkaSource{
+		logger:     logger,
+		sourceName: sourceName,
+		sourceURN:  sourceURN,
+		consumer:   consumer,
+		topic:      x.Topic,
+		channels:   map[int32]chan *kafka.Message{}, // partition -> messages
+		wg:         &sync.WaitGroup{},
+		process:    process,
 	}
 
 	if err = consumer.Subscribe(x.Topic, func(consumer *kafka.Consumer, event kafka.Event) error {
-		logger.Info("re-balance", "event", event.String())
-		switch e := event.(type) {
-		case kafka.AssignedPartitions:
-			for _, p := range e.Partitions {
-				assignedPartition(p.Partition)
-			}
-		case kafka.RevokedPartitions:
-			for _, p := range e.Partitions {
-				revokedPartition(p.Partition)
-			}
-		}
-		return nil
+		return s.rebalanced(ctx, event)
 	}); err != nil {
 		return nil, err
 	}
 
-	go wait.JitterUntilWithContext(ctx, func(context.Context) {
-		logger.Info("starting poll loop")
-		for {
-			// shutdown will be blocked for the amount of time we specify here
-			ev := consumer.Poll(5 * 1000)
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				switch e := ev.(type) {
-				case *kafka.Message:
-					func() {
-						defer func() {
-							// Fact 1 - if you send a message on a closed channel, you get a panic.
-							// Fact 2 - it is impossible to know if a channel is close in Golang.
-							// we need to recover any panic, so we don't pollute the logs
-							if r := recover(); r != nil {
-								logger.Info("recovered from panic while queuing message", "recover", fmt.Sprint(r))
-							}
-						}()
-						channels[e.TopicPartition.Partition] <- e
+	go wait.JitterUntilWithContext(ctx, s.startPollLoop, 3*time.Second, 1.2, true)
+
+	return s, nil
+}
+
+func (s *kafkaSource) processMessage(ctx context.Context, msg *kafka.Message) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, fmt.Sprintf("kafka-source-%s", s.sourceName))
+	defer span.Finish()
+	return s.process(
+		dfv1.ContextWithMeta(
+			ctx,
+			dfv1.Meta{
+				Source: s.sourceURN,
+				ID:     fmt.Sprintf("%d-%d", msg.TopicPartition.Partition, msg.TopicPartition.Offset),
+				Time:   msg.Timestamp.Unix(),
+			},
+		),
+		msg.Value,
+	)
+}
+
+func (s *kafkaSource) assignedPartition(ctx context.Context, partition int32) {
+	logger := s.logger.WithValues("partition", partition)
+	if _, ok := s.channels[partition]; !ok {
+		logger.Info("assigned partition")
+		s.channels[partition] = make(chan *kafka.Message, 256)
+		go wait.JitterUntilWithContext(ctx, func(ctx context.Context) {
+			s.consumePartition(ctx, partition)
+		}, 3*time.Second, 1.2, true)
+	}
+}
+
+func (s *kafkaSource) revokedPartition(partition int32) {
+	if _, ok := s.channels[partition]; ok {
+		s.logger.Info("revoked partition", "partition", partition)
+		close(s.channels[partition])
+		delete(s.channels, partition)
+	}
+}
+
+func (s *kafkaSource) startPollLoop(ctx context.Context) {
+	s.logger.Info("starting poll loop")
+	for {
+		// shutdown will be blocked for the amount of time we specify here
+		ev := s.consumer.Poll(5 * 1000)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			switch e := ev.(type) {
+			case *kafka.Message:
+				func() {
+					defer func() {
+						// Fact 1 - if you send a message on a closed channel, you get a panic.
+						// Fact 2 - it is impossible to know if a channel is close in Golang.
+						// we need to recover any panic, so we don't pollute the logs
+						if r := recover(); r != nil {
+							s.logger.Info("recovered from panic while queuing message", "recover", fmt.Sprint(r))
+						}
 					}()
-				case kafka.Error:
-					logger.Error(fmt.Errorf("%v", e), "poll error")
-				case nil:
-					// noop
-				default:
-					logger.Info("ignored event", "event", ev)
-				}
+					s.channels[e.TopicPartition.Partition] <- e
+				}()
+			case kafka.Error:
+				s.logger.Error(fmt.Errorf("%v", e), "poll error")
+			case nil:
+				// noop
+			default:
+				s.logger.Info("ignored event", "event", ev)
 			}
 		}
-	}, 3*time.Second, 1.2, true)
-	return &kafkaSource{
-		logger:   logger,
-		consumer: consumer,
-		topic:    x.Topic,
-		channels: channels,
-		wg:       wg,
-	}, nil
+	}
 }
 
 func (s *kafkaSource) Close() error {
@@ -194,4 +178,40 @@ func (s *kafkaSource) GetPending(context.Context) (uint64, error) {
 		}
 	}
 	return uint64(pending), nil
+}
+
+func (s *kafkaSource) rebalanced(ctx context.Context, event kafka.Event) error {
+	s.logger.Info("re-balance", "event", event.String())
+	switch e := event.(type) {
+	case kafka.AssignedPartitions:
+		for _, p := range e.Partitions {
+			s.assignedPartition(ctx, p.Partition)
+		}
+	case kafka.RevokedPartitions:
+		for _, p := range e.Partitions {
+			s.revokedPartition(p.Partition)
+		}
+	}
+	return nil
+}
+
+func (s *kafkaSource) consumePartition(ctx context.Context, partition int32) {
+	logger := s.logger.WithValues("partition", partition)
+	logger.Info("consuming partition")
+	s.wg.Add(1)
+	defer func() {
+		logger.Info("done consuming partition")
+		s.wg.Done()
+	}()
+	for msg := range s.channels[partition] {
+		offset := int64(msg.TopicPartition.Offset)
+		logger := logger.WithValues("offset", offset)
+		if err := s.processMessage(ctx, msg); err != nil {
+			logger.Error(err, "failed to process message")
+		} else {
+			if _, err := s.consumer.CommitMessage(msg); err != nil {
+				logger.Error(err, "failed to commit message")
+			}
+		}
+	}
 }
