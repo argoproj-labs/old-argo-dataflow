e7aca11dc96f6db943ae73126ac612275266cf4b
Alex Collins <alex_collins@intuit.com>
2021-10-02 16:02:05 -0700

    2s

--- test/kafka-fmea/kafka_fmea_test.go
+++ test/kafka-fmea/kafka_fmea_test.go
@@ -22,8 +22,6 @@ func TestKafkaFMEA_PodDeletedDisruption(t *testing.T) {
 	topic := CreateKafkaTopic()
 	sinkTopic := CreateKafkaTopic()
 
-	time.Sleep(20*time.Second)
-
 	name := CreatePipeline(Pipeline{
 		ObjectMeta: metav1.ObjectMeta{GenerateName: "kafka-"},
 		Spec: PipelineSpec{
