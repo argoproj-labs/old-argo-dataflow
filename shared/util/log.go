f318ebf4ddc4ede41fa22b0660427430defef5ca
Alex Collins <alex_collins@intuit.com>
2021-10-05 12:59:58 -0700

    feat: send error logs to stderr

--- shared/util/log.go
+++ shared/util/log.go
@@ -1,15 +1,33 @@
 package util
 
 import (
-	"os"
-
 	"github.com/bombsimon/logrusr"
 	"github.com/go-logr/logr"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/writer"
+	"io"
+
+	"os"
 )
 
 func NewLogger() logr.Logger {
-	l := logrus.New()
-	l.SetOutput(os.Stdout)
+	l := log.New()
+	l.SetOutput(io.Discard)
+	l.AddHook(&writer.Hook{
+		Writer: os.Stderr,
+		LogLevels: []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+			log.ErrorLevel,
+			log.WarnLevel,
+		},
+	})
+	l.AddHook(&writer.Hook{
+		Writer: os.Stdout,
+		LogLevels: []log.Level{
+			log.InfoLevel,
+			log.DebugLevel,
+		},
+	})
 	return logrusr.NewLogger(l)
 }
