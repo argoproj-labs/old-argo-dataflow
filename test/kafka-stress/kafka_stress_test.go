5301aa74669716af4c2714a1f2682d2171e4dd01
Alex Collins <alex_collins@intuit.com>
2021-10-08 10:00:50 -0700

    test: remove pending check

--- test/kafka-stress/kafka_stress_test.go
+++ test/kafka-stress/kafka_stress_test.go
@@ -106,8 +106,6 @@ func testKafkaSinkStress(t *testing.T, async bool) {
 	defer StartTPSReporter(t, "main", prefix, n)()
 
 	go PumpKafkaTopic(topic, n, prefix, Params.MessageSize)
-	WaitForPending(Params.Timeout)
-	WaitForNothingPending(Params.Timeout)
 	WaitForTotalSourceMessages(n, Params.Timeout)
 	WaitForTotalSunkMessages(n, Params.Timeout)
 }
