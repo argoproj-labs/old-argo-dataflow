3a40545e374c3b102a736b5d5723d3c75a5195a1
Derek Wang <whynowy@gmail.com>
2021-10-13 13:51:14 -0700

    test case
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- manager/controllers/scaling/scaling_test.go
+++ manager/controllers/scaling/scaling_test.go
@@ -90,6 +90,7 @@ func TestRequeueAfter(t *testing.T) {
 			},
 		})
 		assert.NoError(t, err)
-		assert.Equal(t, 4*time.Minute, requeueAfter)
+		assert.True(t, requeueAfter.Seconds() > 191)
+		assert.True(t, requeueAfter.Seconds() < 289)
 	})
 }
